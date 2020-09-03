806417df706d2e9face46b7bf0e7f9a333e6cd5f
Claudio Filho <claudioasfilho@gmail.com>
2020-09-03 14:34:18 -0700

    Modified the files, in order to be able to select the target devices

---
+++ MG22toBG22f.py
@@ -0,0 +1,89 @@
+import os, shutil
+from os import listdir
+retval = os.getcwd()
+print "Current working directory %s" % retval
+
+def replaceXG2(file, device):
+    devicelower = device.lower()
+    fin = open(file, "rt")
+    data = fin.read()
+    data = data.replace('EFR32MG22C224F512IM40', device)
+    data = data.replace('efr32mg22c224f512im40', devicelower)
+    data = data.replace('MG22', 'BG22')
+    data = data.replace('mg22', 'bg22')
+    fin.close()
+
+    fin = open(file, "wt")
+    fin.write(data)
+    fin.close()
+    print "%s changed" % file
+
+def changedir(directory):
+    curDir = os.getcwd()
+    os.chdir(curDir + directory)
+    curDir = os.getcwd()
+    print "Current working directory %s" % curDir
+
+def list_files1(directory, extension):
+    return (f for f in listdir(directory) if f.endswith('.' + extension))
+
+def printDevices():
+    print "Choose which device do you want to convert your project to: \n\r"
+    for key, value in parts.items():
+        print key,':',value
+
+
+parts = {
+    "A" : "EFR32BG22C112F352GM32",
+    "B" : "EFR32BG22C222F352GM32",
+    "C" : "EFR32BG22C222F352GM40",
+    "D" : "EFR32BG22C222F352GN32",
+    "E" : "EFR32BG22C224F512GM32",
+    "F" : "EFR32BG22C224F512GM40",
+    "G" : "EFR32BG22C224F512GN32",
+    "H" : "EFR32BG22C224F512IM32",
+    "I" : "EFR32BG22C224F512IM40"
+}
+
+printDevices()
+result = None
+while result is None:
+    val = raw_input ("Enter your choice: ")
+    if (val.islower()) == True:
+        val = val.upper()
+    if val in parts:
+        device = parts.get(val)
+        print "Device chosen %s " % device
+        result = 1
+    else:
+        print "Choice not available \n\r"
+
+
+replaceXG2(".project", device)
+replaceXG2(".cproject", device)
+
+#Searches for specific file extentions and assign them to variables
+x = os.listdir(os.getcwd())
+
+for i in x:
+    if i.endswith(".slcp"):
+        SLCP = i
+        print(SLCP)
+    if i.endswith(".slps"):
+        SLPS = i
+        print(SLPS)
+    if i.endswith(".pintool"):
+        PINTOOL = i
+        print(PINTOOL)
+
+
+replaceXG2(SLCP, device)
+replaceXG2(SLPS, device)
+replaceXG2(PINTOOL, device)
+
+#It checks if there is a compiler output folder and removes it
+if os.path.isdir(os.getcwd()+ "/GNU ARM v7.2.1 - Debug"):
+    shutil.rmtree("GNU ARM v7.2.1 - Debug")
+    print "Compiler Output Directory removed"
+else:
+    print "Compiler Output Directory not present"
--- README.md
+++ README.md
@@ -4,10 +4,15 @@ Silicon Labs are now officially only making MG2x RF cards for our WSTK base boar
 
 This python script was created to help you migrate your MG2x project, developed with the WSTK to a BG2x end device.
 
-In order to use it, you just have to add it to the project folder and then run on the command line:
-
-python MG21toBG21.py
-
-Ideally you want to have your project closed on Simplicity Studio 5. As you reopen it, it will be converted to a BG2x part.
+In order to use it, please go through the following steps:
+1) Create your project using the MG2x board on Simplicity Studio 5 (SSv5)
+2) Add all the peripherals required and relevant software additions. Keep in mind if the end device has a different pin count, the GPIOs labels chosen need to be also available on that particular device.
+3) Add the python scrip to the main project folder
+4) Close the project on SSv5
+4) Open the project folder on Command line and run python MG21toBG21.py
+5) It will give you the option to choose which is the device needed and then it will change all the files related to it.
+6) As you reopen the project on SSv5, it will be converted to a BG2x part.
 
 This was tested with Python 3.85
+
+Known bugs: there is an issue on the configurator
